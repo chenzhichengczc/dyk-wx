84147fd3a4758ea4f8e9e1040bae7c5101066c18
chenzhichengczc <178642170@qq.com>
2019-08-29 18:25:10 +0800

    更新接口信息

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch: function() {
     //调用API从本地缓存中获取数据
     const that = this;
     that.urls();
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs);
     wx.getSystemInfo({
       success: function(res) {
-      that.systemInfo = res;
+        that.systemInfo = res;
       }
     })
+
+    //获取授权信息
+    that.login();
+
   },
 
   siteInfo: require("config.js"),
 
-  urls: function () {
+  urls: function() {
     var that = this;
     that.globalData.urls = that.siteInfo.config.url + that.siteInfo.config.subDomain;
   },
 
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
+  login: function() {
+    var that = this;
+
+    wx.login({
+      success(res) {
+        if (res.code) {
+          wx.request({
+            url: that.globalData.urls + '/user/getUserInfo',
+            data: {
+              appid: "wxfd945e2b4a767dc3",
+              secret: "0d921f83d3a88312703d8771709fb7e1",
+              js_code: res.code,
+              grantType: 'authorization_code',
+
+            },
+            method: "POST",
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            success: function(res) {
+              if (res.data.code == 500) {
+                that.globalData.usinfo = 0;
+                return;
+              }
+              if (res.data.code != 0) {
+                wx.hideLoading();
+                wx.showModal({
+                  title: "提示",
+                  content: "无法登录，请重试",
+                  showCancel: false
+                });
+                return;
+              }
+              that.globalData.openId = res.data.data.openid;
+              that.globalData.token = res.data.data.access_token;
             }
           })
         }
-      })
-    }
+      }
+    })
   },
-  globalData:{
-    userInfo:null
+
+
+  globalData: {
+    userInfo: null
   },
-  systemInfo:null,
-  
+  systemInfo: null,
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,8 @@ Page({
   data: {
     width: app.systemInfo.windowWidth,
     height: app.systemInfo.windowHeight,
-    wxlogin: false,
-    toplogo : "/images/logo.png",
+    wxlogin: true,
+    toplogo: "/images/logo.png",
     banner: ['http://i.dxlfile.com/adm/material/2016_12_12/20161212135600242250.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/2017010411165785666.jpg',
       'http://i.dxlfile.com/adm/material/2017_01_04/20170104140739205869.jpg',
@@ -56,7 +56,27 @@ Page({
   },
 
   onLoad: function() {
-
+    var that = this;
+    wx.hideTabBar();
+    wx.getSetting({
+      success: (res) => {
+        if (res.authSetting['scope.userInfo']) {//授权了，可以获取用户信息了
+          wx.getUserInfo({
+            success: (res) => {
+              that.setData({
+                wxlogin: true
+              })
+              wx.showTabBar();
+            }
+          })
+        } else {//未授权，跳到授权页面
+          that.setData({
+            wxlogin: false
+          })
+          wx.hideTabBar();
+        }
+      }
+    })
   },
 
   fucClick(event) {
@@ -81,6 +101,61 @@ Page({
     }
 
   },
+
+
+  //授权通过信息存储
+  userlogin: function(e) {
+    var that = this;
+    //没有授权用户信息 , 拒绝
+    if (!e.detail.rawData) {
+      wx.showToast({
+        title: '授权失败，请授权使用',
+        icon: "none",
+        duration: 3000
+      })
+      return;
+    }
+    
+    wx.request({
+      url: app.globalData.urls + '/api/user/decodeUserInfo',
+      data: {
+        openId: app.globalData.openId, //用户的唯一标识
+        nickName: e.detail.userInfo.nickName, //微信昵称
+        avatarUrl: e.detail.userInfo.avatarUrl, //微信头像
+        province: e.detail.userInfo.province, //用户注册的省
+        city: e.detail.userInfo.city, //用户注册的市
+        gender: e.detail.userInfo.gender, //用户性别
+        country: e.detail.userInfo.country //用户所在国家
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded', // 默认值
+        'token': app.globalData.token
+      },
+      method: "post",
+      success: function (e) {
+        if (e.data.code == 0) {
+          wx.showToast({
+            title: '授权已经成功',
+            mask: true,
+            duration: 2000,
+            success: function () {
+              that.setData({
+                wxlogin: true
+              })
+              wx.showTabBar();
+            }
+          })
+        }else{
+          wx.showModal({
+            title: '授权通知',
+            content: '为了体验建议告诉我们您的微信公开信息，绝对不会泄露隐私噢！',
+          })
+        }
+      }
+    })
+  },
+
+
   goodDetail(event) {
     wx.navigateTo({
       url: '',
